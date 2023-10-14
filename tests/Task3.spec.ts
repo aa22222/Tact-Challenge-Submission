78bc1e69df9ab89afdf52845b4e452a7f15249e6
Luke Wu <lukewu28@gmail.com>
2023-10-14 17:06:41 -0500

    'idk'

--- tests/Task3.spec.ts
+++ tests/Task3.spec.ts
@@ -1,21 +1,35 @@
 import { Blockchain, SandboxContract, TreasuryContract, } from '@ton-community/sandbox';
-import { toNano } from 'ton-core';
+import { toNano, Address, beginCell } from 'ton-core';
 import { JettonMaster } from 'ton';
-import { Task3 } from '../wrappers/Task3';
+import { Task3, TokenNotification } from '../wrappers/Task3';
+import '@ton-community/test-utils';
 
+function tokenNotification(amount: bigint, from: Address) : TokenNotification {
+    return {
+        $$type: 'TokenNotification',
+        queryId: 1n,
+        amount: amount,
+        from: from,
+        forwardPayload: beginCell().endCell()
+    };
+}
 describe('Task3', () => {
     let blockchain: Blockchain;
     let task3: SandboxContract<Task3>;
     let user: SandboxContract<TreasuryContract>;
-    let jettonA, jettonB: SandboxContract<JettonMaster>;
+    let jettonA: SandboxContract<TreasuryContract>;
+    let jettonB: SandboxContract<TreasuryContract>;
+    let deployer: SandboxContract<TreasuryContract>;
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
         
+        user = await blockchain.treasury("user");
+        jettonA = await blockchain.treasury("jettonA");
+        jettonB = await blockchain.treasury("jettonB");
+        deployer = await blockchain.treasury('deployer');
         
-        
-        task3 = blockchain.openContract(await Task3.fromInit());
-        const deployer = await blockchain.treasury('deployer');
+        task3 = blockchain.openContract(await Task3.fromInit(deployer.address, jettonA.address, jettonB.address));
         
         const deployResult = await task3.send(
             deployer.getSender(),
@@ -35,7 +49,40 @@ describe('Task3', () => {
         });
     });
 
-    it('test', async () => {
+    it('Deposit Coins', async () => {
+        const aRes = await task3.send(
+            jettonA.getSender(),
+            {
+                value: toNano('0.05')
+            },
+            tokenNotification(10000n, deployer.address)
+        );
+        const bRes = await task3.send(
+            jettonB.getSender(),
+            {
+                value: toNano('0.05')
+            },
+            tokenNotification(5000n, deployer.address)
+        );
+        console.log([
+            await task3.getBalance(jettonA.address),
+            await task3.getBalance(jettonB.address),
+        ]);
+
+        
+        const swapAforB = await task3.send(
+            jettonA.getSender(),
+            {
+                value: toNano('0.05')
+            },
+            tokenNotification(10n, user.address)
+        );
+
+        console.log([
+            await task3.getBalance(jettonA.address),
+            await task3.getBalance(jettonB.address),
+        ]);
+
     });
 });
 
