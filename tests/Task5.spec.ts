f3b9001d5a590bb3e50d67e3da2aee5f9f255ec2
Luke Wu <lukewu28@gmail.com>
2023-10-16 20:54:12 -0500

    fix 4?

--- tests/Task5.spec.ts
+++ tests/Task5.spec.ts
@@ -9,7 +9,7 @@ import "@ton-community/test-utils";
 describe('Task5', () => {
     let blockchain: Blockchain;
     let task5: SandboxContract<Task5>;
-    let nftCollection: SandboxContract<any>;
+    let nftCollection: SandboxContract<TreasuryContract>;
     let deployer: SandboxContract<TreasuryContract>;
 
     async function transferNft(nft: SandboxContract<NftItem>, from: SandboxContract<TreasuryContract>, to: SandboxContract<any>, value: bigint ) : Promise<SendMessageResult> {
@@ -31,7 +31,10 @@ describe('Task5', () => {
     async function createNft(index: bigint, owner: SandboxContract<any>){
         const nft = blockchain.openContract(
             await NftItem.fromInit(nftCollection.address, index, nftCollection.address, beginCell().endCell()));
-        await transferNft(nft, nftCollection, owner, 0n);
+        const re = await transferNft(nft, nftCollection, owner, toNano("1.0"));
+        console.log((await nft.getGetNftData()).owner_address)
+        console.log(re.transactions.map(e => e.inMessage?.info))
+        
         return nft;
     }
 
@@ -58,24 +61,31 @@ describe('Task5', () => {
 
     it('Dont sent enough TON', async () => {
         const user = await blockchain.treasury("user", {balance : toNano("100.00")});
-        const nftU = await createNft(0n, user);
-        const nftA = await createNft(1n, deployer);
-        expect((await nftA.getGetNftData()).owner_address).toEqualAddress(deployer.address);
-        expect((await nftU.getGetNftData()).owner_address).toEqualAddress(user.address);
+        const nfts = [];
+        for(let i = 0 ; i< 10; i++){
+            let nft = await createNft(BigInt(i), deployer.address);
+            expect((await nft.getGetNftData()).owner_address).toEqualAddress(deployer.address);
+            const r = await transferNft(nft, deployer, task5, toNano("1"));
+            nfts.push(nft);
+        }
+        
+        for(const nft of nfts){
+            expect((await nft.getGetNftData()).owner_address).toEqualAddress(task5.address);
+        }
 
-        const res = await transferNft(nftA, deployer, task5, toNano("0.05"));
-        // console.log(res.transactions.map(e => e.inMessage?.info));
-        // console.log(res.transactions.map(e => e.description));
-        expect((await nftA.getGetNftData()).owner_address).toEqualAddress(task5.address);
+        console.log(await task5.getNfts());
+        const res = await task5.send(
+            deployer.getSender(),
+            { value: toNano("10")},
+            {
+                $$type: "AdminWithdrawalAllNFTs",
+                queryId: 10n
+            }    
+        )
 
-        const res2 = await transferNft(nftU, user, task5, toNano("1.1"));
-        // console.log(await user.getBalance())
-        // console.log(res2.transactions.map(e => e.inMessage?.info));
-        // console.log(res2.transactions.map(e => e.debugLogs));
-        // console.log(res2.transactions.map(e => e.description));
-        // expect((await nftA.getGetNftData()).owner_address).toEqualAddress(user.address);
-        console.log(res2.transactions.map(e => [e.inMessage?.info, e.inMessage?.body]))
-        console.log([user, deployer, nftA, nftU, task5].map(e => e.address).join('\n'))
+        for(const nft of nfts){
+            expect((await nft.getGetNftData()).owner_address).toEqualAddress(deployer.address);
+        }
     });
 });
 
